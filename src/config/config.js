37d1ede46378cd7e91761f24f92d0d8e6438eecf
Javystik <miroivladik@gmail.com>
2025-06-04 18:07:40 +0300

    cimit

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080';
-// export const API_BASE_URL = 'https://catopia-back-crgxdxavhkg7g6ew.polandcentral-01.azurewebsites.net';
+//export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://catopia-back-crgxdxavhkg7g6ew.polandcentral-01.azurewebsites.net';
 export const MINIMUM_LOADING_TIME = 1000;
